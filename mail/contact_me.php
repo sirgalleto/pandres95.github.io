79c1a2a5e75605c9e57de82edd16f31a85377812
Pablo Dorado <pandres95@boolinc.co>
2014-09-23 15:50:11 -0500

    Fix headers

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,14 +1,12 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
-   {
-	echo "No arguments Provided!";
-	return false;
-   }
+if(empty($_POST['name']) || empty($_POST['email']) || empty($_POST['phone']) ||
+   empty($_POST['message'])	|| !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL)) {
+
+       echo "No arguments Provided!";
+       return false;
+
+}
 
 $name = $_POST['name'];
 $email_address = $_POST['email'];
@@ -16,22 +14,28 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // Create the email and send the message
-$to = 'Pablo Dorado <pandres95@boolinc.co>'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
-$email_subject = "Website Contact Form:  $name";
+$me = 'Pablo Dorado <pandres95@boolinc.co>';
+$subject = "Website Contact Form: $name";
 
-$email_body = "You have received a new message from your website contact form.\n\n" .
-            "Here are the details:\n\n" .
-            "Name: $name\n\n" .
-            "Email: $email_address\n\n" .
-            "Phone: $phone\n\n" .
-            "Message:\n$message";
+$body = "You have received a new message from your website contact form.\n\n" .
+        "Here are the details:\n\n" .
+        "Name: $name\n\n" .
+        "Email: $email_address\n\n" .
+        "Phone: $phone\n\n" .
+        "Message:\n$message";
 
-$headers = "From: Pablo Dorado <pandres95@boolinc.co>\r\n";
-$headers .= "Reply-To: $email_address";
+$headers   = array();
+$headers[] = "MIME-Version: 1.0";
+$headers[] = "Content-type: text/plain; charset=iso-8859-1";
+$headers[] = "From: Pablo Dorado <pandres95@boolinc.co>";
+$headers[] = "Reply-To: {$name} <{$email_address}>";
+$headers[] = "Subject: {$subject}";
+$headers[] = "X-Mailer: PHP/".phpversion();
 
-$var = mail($to,$email_subject,$email_body,$headers);
+$result = new Class();
+$result->sent = mail($me, $subject, $body, implode('\r\n',$headers));
 
-print(json_encode($var));
+print(json_encode($result));
 return true;
 
 ?>
