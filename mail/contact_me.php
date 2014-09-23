9ba5b1069e4a10d877133b1880172bc863927ee5
Pablo Dorado <pandres95@boolinc.co>
2014-09-23 15:36:37 -0500

    Log mail info

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -16,11 +16,22 @@ $phone = $_POST['phone'];
 $message = $_POST['message'];
 
 // Create the email and send the message
-$to = 'pandres95@boolinc.co'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$to = 'Pablo Dorado <pandres95@boolinc.co>'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
-$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
-$headers = "From: noreply@pandres95.boolinc.co\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+
+$email_body = "You have received a new message from your website contact form.\n\n" .
+            "Here are the details:\n\n" .
+            "Name: $name\n\n" .
+            "Email: $email_address\n\n" .
+            "Phone: $phone\n\n" .
+            "Message:\n$message";
+
+$headers = "From: Pablo Dorado <pandres95@boolinc.co>\n";
 $headers .= "Reply-To: $email_address";
-mail($to,$email_subject,$email_body,$headers);
+
+$var = mail($to,$email_subject,$email_body,$headers);
+
+print(json_encode($var));
 return true;
+
 ?>
