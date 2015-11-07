54f8285a238e5ece38b12d7c5f8171ae0059802a
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-11-07 13:37:17 -0500

    Install gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
 'use strict';
 
 var sequence    = require('gulp-sequence')
-,   gulp        = require('./gulp-load')('tasks');
+,   gulp        = require('gulp-folder-load')('tasks');
 
 gulp.task('prepare', sequence('symlink', 'wiredep', 'inject'));
 gulp.task('default', sequence('prepare'));
