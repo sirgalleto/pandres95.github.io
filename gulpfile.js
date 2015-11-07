81eb6f03e7d09d5ad39fc19de109e0215448da5f
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-11-07 15:42:50 -0500

    Group same-file tasks

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,6 @@
 var sequence    = require('gulp-sequence')
 ,   gulp        = require('gulp-folder-load')('tasks');
 
-gulp.task('prepare', sequence('template', 'symlink', [ 'wiredep', 'inject' ]));
+gulp.task('prepare', sequence(['template', 'symlink'], ['wiredep', 'inject']));
 gulp.task('default', sequence('prepare'));
-gulp.task('build', sequence('prepare', 'assets', 'fonts'));
+gulp.task('build', sequence('prepare', ['assets', 'fonts']));
