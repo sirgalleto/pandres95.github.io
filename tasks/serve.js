767e7ddfee8bd34f93be5be0a1ee43c0e9ac7516
Pablo Andrés Dorado Suárez <im@pandres95.me>
2016-08-15 05:15:01 -0500

    Add now-listening component.

--- tasks/serve.js
+++ tasks/serve.js
@@ -2,6 +2,11 @@ module.exports = (gulp) => {
     'use strict';
     const   server  = require('browser-sync')
     ,       spa     = require('browser-sync-spa');
+    const httpProxy = require('http-proxy');
+
+    let proxy = httpProxy.createProxyServer({
+        target: 'https://pandres95.me', changeOrigin: true, secure: true
+    });
 
     return () => {
         server.use(spa());
@@ -9,6 +14,11 @@ module.exports = (gulp) => {
         server.init({
             server: {
                 baseDir: 'dev'
+            },
+            middleware: function (req, res, next) {
+                if (req.url.match(/^\/api/)) {
+                    proxy.web(req, res);
+                } else next();
             },
             startPath: 'index.html',
             ghostMode: false,
