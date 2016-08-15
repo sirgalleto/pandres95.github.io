665018b9e5d837d8e06efa8df89eff0817bc6f27
Pablo Andrés Dorado Suárez <im@pandres95.me>
2016-08-15 02:35:36 -0500

    Add serve task

---
+++ tasks/serve.js
@@ -0,0 +1,22 @@
+module.exports = (gulp) => {
+    'use strict';
+    const   server  = require('browser-sync')
+    ,       spa     = require('browser-sync-spa');
+
+    return () => {
+        server.use(spa());
+
+        server.init({
+            server: {
+                baseDir: 'dev'
+            },
+            startPath: 'index.html',
+            ghostMode: false,
+            notify: false,
+            debounce: 200,
+            port: 8000
+        });
+
+        gulp.watch('dev/index.html').on('change', server.reload);
+    };
+};
