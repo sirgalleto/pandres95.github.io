ee567e137cfa951306ae891f85facda0b0d4d6df
Pablo Andrés Dorado Suárez <im@pandres95.me>
2016-08-15 02:35:54 -0500

    Gulp–Update tasks

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var sequence    = require('gulp-sequence')
 ,   gulp        = require('gulp-folder-load')('tasks');
 
 gulp.task('prepare', sequence(['template', 'symlink'], 'wiredep', 'inject'));
-gulp.task('default', sequence('prepare'));
+gulp.task('default', sequence('prepare', 'serve'));
 gulp.task(
     'build', sequence('prepare', ['assets', 'fonts'], 'source', 'components')
 );
