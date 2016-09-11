6c2c2ffba19584cc362a170fddee6fbcd0adb046
Pablo Andrés Dorado Suárez <im@pandres95.me>
2016-09-11 15:31:00 -0500

    Add Task: Copy other fonts, but open-sans-fontface

--- tasks/fonts.js
+++ tasks/fonts.js
@@ -5,6 +5,13 @@ module.exports = (gulp) => {
     return () => {
         return (gulp
             .src('./dev/lib/open-sans-fontface/fonts/**')
-        ).pipe(gulp.dest('./dist/styles/fonts'));
+        ).pipe(gulp.dest('./dist/styles/fonts')),
+        (gulp
+            .src([
+                './dev/lib/**/fonts/**', '!./dev/lib/open-sans-fontface',
+                '!./dev/lib/open-sans-fontface/fonts/**'
+            ])
+            .pipe(flatten())
+        ).pipe(gulp.dest('./dist/fonts'));
     };
 };
